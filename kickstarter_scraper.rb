5232560e6321f9af5609fe8005f80885d8ee5791
Teerth Patel <teerth1205@gmail.com>
2020-02-07 02:49:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,7 +6,13 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  projects = {}
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {:image_link => }
+  end 
+  projects
+    
 end
 
 create_project_hash
